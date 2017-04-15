4210b08f6aaaff5f565df1bd2ea501d580295211
andela-sakande <suraj.akande@getitllc.com>
2017-04-15 09:48:48 +0100

    Add landing page

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,64 +5,15 @@
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>Laravel</title>
+        <title>Inayat Thrift</title>
 
         <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css?family=Raleway:100,600" rel="stylesheet" type="text/css">
+        <link rel="stylesheet" type="text/css" href="css/app.css">
+        <link rel="stylesheet" type="text/css" href="css/style.css">
 
-        <!-- Styles -->
-        <style>
-            html, body {
-                background-color: #fff;
-                color: #636b6f;
-                font-family: 'Raleway', sans-serif;
-                font-weight: 100;
-                height: 100vh;
-                margin: 0;
-            }
-
-            .full-height {
-                height: 100vh;
-            }
-
-            .flex-center {
-                align-items: center;
-                display: flex;
-                justify-content: center;
-            }
-
-            .position-ref {
-                position: relative;
-            }
-
-            .top-right {
-                position: absolute;
-                right: 10px;
-                top: 18px;
-            }
-
-            .content {
-                text-align: center;
-            }
-
-            .title {
-                font-size: 84px;
-            }
-
-            .links > a {
-                color: #636b6f;
-                padding: 0 25px;
-                font-size: 12px;
-                font-weight: 600;
-                letter-spacing: .1rem;
-                text-decoration: none;
-                text-transform: uppercase;
-            }
-
-            .m-b-md {
-                margin-bottom: 30px;
-            }
-        </style>
+        <script type="text/javascript" src="js/app.js" ></script>
+        <script type="text/javascript" src="js/script.js"></script>
     </head>
     <body>
         <div class="flex-center position-ref full-height">
@@ -78,16 +29,30 @@
             @endif
 
             <div class="content">
-                <div class="title m-b-md">
-                    Laravel
+                <div class="title">
+                    <b>Al-Inayat Credit & Thrift Society</b>
                 </div>
 
-                <div class="links">
-                    <a href="https://laravel.com/docs">Documentation</a>
-                    <a href="https://laracasts.com">Laracasts</a>
-                    <a href="https://laravel-news.com">News</a>
-                    <a href="https://forge.laravel.com">Forge</a>
-                    <a href="https://github.com/laravel/laravel">GitHub</a>
+                <div class="row">
+                <div class="col-md-12">
+                  <section class="login-form">
+                    <form method="post" action="#" role="login">
+                      <input type="phone" name="phone" placeholder="Phone Number" required class="form-control input-lg" value="" />
+
+                      <input type="password" class="form-control input-lg" id="password" placeholder="Password" required="" />
+
+
+                      <div class="pwstrength_viewport_progress"></div>
+
+
+                      <button type="submit" name="go" class="btn btn-lg btn-primary btn-block">Sign in</button>
+                      <div>
+                         <a href="#">Reset password</a>
+                      </div>
+
+                    </form>
+                  </section>
+                  </div>
                 </div>
             </div>
         </div>
