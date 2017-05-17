58dddc7e705328b906eddb52a6c2d6ff3fb51f81
Surajudeen Akande <surajudeen.akande@fire-engine-red.com>
2017-05-17 21:49:05 +0100

    Add button for mobile header

--- resources/views/layout/header.blade.php
+++ resources/views/layout/header.blade.php
@@ -1,6 +1,11 @@
 <nav class="navbar navbar-inverse">
     <div class="container-fluid">
         <div class="navbar-header">
+            <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-inverse-collapse">
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+            </button>
             <a class="navbar-brand" href="{{ route('dashboard') }}">Al-inayat</a>
         </div>
         <ul class="nav navbar-nav">
