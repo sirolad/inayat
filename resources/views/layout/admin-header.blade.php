97bd00bd3ccb016aa32fbe199afcd04d9dca14e1
Surajudeen Akande <surajudeen.akande@fire-engine-red.com>
2017-05-17 22:13:53 +0100

    use correct id to toggle navbar

--- resources/views/layout/admin-header.blade.php
+++ resources/views/layout/admin-header.blade.php
@@ -1,7 +1,7 @@
 <nav class="navbar navbar-default navbar-fixed-top">
     <div class="container-fluid">
         <div class="navbar-header">
-            <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-inverse-collapse">
+            <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-nav">
                 <span class="icon-bar"></span>
                 <span class="icon-bar"></span>
                 <span class="icon-bar"></span>
